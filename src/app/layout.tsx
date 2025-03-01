fffd1dc50861806103783ee9ccdeacdd201ef2a5
Jayden Nawotka <156985453+qubered@users.noreply.github.com>
2025-03-02 01:38:45 +1100

    Set default theme to dark

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
       <body className={GeistSans.className}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
         >
           <ClerkThemeWrapper>
