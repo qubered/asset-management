e7ddc7e5eea7b014b7fb0d7fd89fe06fab5ea4ed
Jayden Nawotka <156985453+qubered@users.noreply.github.com>
2025-03-02 01:24:20 +1100

    Add basic homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,31 @@
 "use client"
+import { ThemeToggle } from "@/components/themes/theme-switcher"
+import { buttonVariants } from "@/components/ui/button"
+import { Database } from "lucide-react";
 import Link from "next/link";
+
 
 export default function HomePage() {
   return (
     <main>
-      <div className="flex flex-col items-center justify-center h-screen gap-4">
-        <h1 className="text-4xl font-bold">Asset Management</h1>
-        <p className="text-lg">A new Theatre focused asset management system</p>
+      <div className="flex p-4 justify-end sticky top-0 bg-background">
+        <ThemeToggle />
       </div>
+      <div className="flex flex-col items-center justify-center h-screen gap-8">
+        <div className="flex flex-col items-center justify-center gap-6">
+          <div className="flex items-center gap-2">
+            <Database className="w-10 h-10"></Database>
+            <h1 className="text-4xl font-bold">Asset Management</h1>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <p className="text-lg">Theatre focused asset management system</p>
+            <p className="text-lg text-muted-foreground">Built by theatre people</p>
+          </div>
+        </div>
+        <Link href="/login" className={buttonVariants({ variant: "default" })}>Get Started</Link>
+      </div>
+
+
     </main>
   );
 }
