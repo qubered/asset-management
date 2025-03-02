32216a295a120723b14042e0e0508d0aa94ea944
Jayden Nawotka <156985453+qubered@users.noreply.github.com>
2025-03-02 16:17:23 +1100

    New Homepage hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,27 +3,29 @@ import { ThemeToggle } from "@/components/themes/theme-switcher"
 import { buttonVariants } from "@/components/ui/button"
 import { Database } from "lucide-react";
 import Link from "next/link";
-
+import { Hero32 } from "@/components/home-hero";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export default function HomePage() {
   return (
-    <main>
-      <div className="flex p-4 justify-end sticky top-0 bg-background">
+    <main className="">
+      <div className="flex fixed top-4 right-4 z-50 gap-4">
         <ThemeToggle />
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+
       </div>
-      <div className="flex flex-col items-center justify-center h-screen gap-8">
-        <div className="flex flex-col items-center justify-center gap-6">
-          <div className="flex items-center gap-2">
-            <Database className="w-10 h-10"></Database>
-            <h1 className="text-4xl font-bold">Asset Management</h1>
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            <p className="text-lg">Theatre focused asset management system</p>
-            <p className="text-lg text-muted-foreground">Built by theatre people</p>
-          </div>
-          <Link href="/main" className={buttonVariants({ variant: "default" })}>Get Started</Link>
-        </div>
-      </div>
+      <Hero32 heading="Asset Management" description="Theatre focused asset management system" button={{ text: (
+        <>
+          <SignedIn>
+            Dashboard
+          </SignedIn>
+          <SignedOut>
+            Login to Get Started
+          </SignedOut>
+        </>
+      ), url: "/main" }} />
     </main>
   );
 }
