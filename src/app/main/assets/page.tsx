cce629c8cb8c2b8bbcf4f025c1f1681e4a0e6650
Jayden Nawotka <156985453+qubered@users.noreply.github.com>
2025-03-07 18:42:45 +1100

    Some typescript fixes to stop type errors

--- src/app/main/assets/page.tsx
+++ src/app/main/assets/page.tsx
@@ -2,15 +2,20 @@
 
 import { columns } from "./columns";
 import { DataTable } from "@/components/data-table";
-import { GetAssets, GetID } from "@/server/convex/dbHelper";
-
+import { GetAssets, GetID, GetLastAsset } from "@/server/convex/dbHelper";
+import { AssetForm } from "./new-asset-form";
 export default function Home() {
   const assets = GetAssets();
+  const lastAsset = GetLastAsset();
   const orgID = GetID();
   return (
     <div>
       <h1>Assets: {orgID}</h1>
+      <p>Latest: {lastAsset?.assetTag}</p>
       <DataTable columns={columns} data={assets ?? []} />
+      <div>
+        <AssetForm />
+      </div>
     </div>
   );
 }
